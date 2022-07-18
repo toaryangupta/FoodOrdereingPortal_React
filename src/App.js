866865fc47f66635acb3d7e6824046c9df177b7d
toaryangupta <toaryangupta@gmail.com>
2022-07-18 18:54:48 +0530

    useContext Working

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Fragment, useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] =useState(false);
@@ -18,13 +19,13 @@ function App() {
   };
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler}/>}
       <Header onShowCard ={showCartHandler}/>
       <main>
         <Meals />
       </main>
-    </Fragment> 
+    </CartProvider> 
   );
 };
 
---
+++ src/store/CartProvider.js
@@ -0,0 +1,55 @@
+import {useReducer} from 'react';
+import CartContext from "./cart-context";
+
+const defaultCartState ={
+    items:[],
+    toatlAmount:0
+}
+
+
+const cartReducer = (state, action) => {
+
+    if(action.type ==='ADD'){
+            const updatedItems =state.items.concat(action.item);
+            const updatedTotalAmount =state.toatlAmount + action.item.price * action.item.amount;
+
+            return {
+                items: updatedItems,
+                totalAmount:updatedTotalAmount
+
+            }
+
+    }
+
+
+    return defaultCartState;
+
+};
+
+const CartProvider = (props) => {
+
+  const [cartState, dispatchCartAction]=useReducer(cartReducer, defaultCartState);
+ 
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({type:'ADD', item:item})
+  };
+
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({type:'REMOVE', id:id})
+
+    
+  };
+  const cartContext = {
+    items: cartState.items,
+    totalItem: cartState.toatlAmount,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
